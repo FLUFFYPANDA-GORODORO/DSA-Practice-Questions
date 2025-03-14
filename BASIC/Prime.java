b500143d9c1433c5b3c887cb0653946a6a24f152
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-03-14 12:04:15 +0530

    Prime numbers in range

--- BASIC/Prime.java
+++ BASIC/Prime.java
@@ -1,19 +1,19 @@
 //Write a program to find all the possible prime numbers within the range
 //Input : Two integers start and end of range
-//Output : All possible prime numbers within this range
+//Output : All possible prime numbers within this range & sum of all prime numbers within these range
 //Test Cases :
-//Input : 1-10  Output 2 3 5 7
-//Input : 1-100  Output 2 3 5 7 11 ......89 97
+//Input : 1-10  Output 2 3 5 7  sum = 17
+//Input : 1-100  Output 2 3 5 7 11 ......89 97  sum = 1060 
 
 
 import java.util.Scanner;
 
-public class Practice6 {
+public class Prime {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int n1,n2;
+        int n1,n2,sum=0;
         System.out.print("Enter starting number of range : ");
         n1 = sc.nextInt();
         System.out.print("Enter ending number of range : ");
@@ -22,8 +22,10 @@ public class Practice6 {
         for(int i = n1 ; i <= n2 ; i++){
             if(isPrime(i)){
                 System.out.print(i + " ");
+                sum += i;
             }
         }
+        System.out.println("\nSum of all prime numbers is : " + sum);
     }
 
     public static boolean isPrime(int num){
@@ -51,3 +53,10 @@ public class Practice6 {
         return false;
     }
 }
+
+// #Output
+// Enter starting number of range : 1
+// Enter ending number of range : 10
+// Prime numbers from range 1 to 10 are : 
+// 2 3 5 7 
+// Sum of all prime numbers is : 17
