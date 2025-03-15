131156d23c9719b43159235fbf652805faab079d
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-03-15 14:04:15 +0530

    Print digits separeted by space

--- Intermediate/SequenceOfDigits.java
+++ Intermediate/SequenceOfDigits.java
@@ -4,7 +4,7 @@
 
 import java.util.Scanner;
 
-public class Practice6 {
+public class SequenceOfDigits {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
