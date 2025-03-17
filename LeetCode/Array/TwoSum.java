6a571884709525294d0f19b225bc1b13d31dbc70
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-03-17 21:54:32 +0530

    Two Sum

---
+++ LeetCode/Array/TwoSum.java
@@ -0,0 +1,19 @@
+// Solution using array method
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[] {i , j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("Invalid Syntax"); 
+        //return null;  
+    }
+}
+
+//Time  Complexity : O(n^2)
+//Space Complexity : O(n)
+
+// ----------------------------------------------------------------
