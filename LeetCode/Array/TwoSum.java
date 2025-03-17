a049157d948118bc8337658fc97d309908dab234
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-03-17 22:11:04 +0530

    Two Sum

--- LeetCode/Array/TwoSum.java
+++ LeetCode/Array/TwoSum.java
@@ -17,3 +17,26 @@ class Solution {
 //Space Complexity : O(n)
 
 // ----------------------------------------------------------------
+// Solution using Hashmap
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> indexMap =  new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            indexMap.put(nums[i],i);
+        }  
+        for(int i = 0; i < nums.length; i++){
+            int n = target - nums[i];
+            int index_of_n = indexMap.getOrDefault(n,-1);
+            if(index_of_n != -1 && index_of_n != i){
+                return new int[]{i,index_of_n};
+            }
+        }
+        return null;
+
+    }
+}
+
+//Time  Complexity : O(n)
+//Space Complexity : O(n)
+
