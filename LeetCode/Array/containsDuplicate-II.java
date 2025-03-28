fdf6a6cd7d3104baad09b19e68a624834ff91259
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-03-28 10:24:39 +0530

    Contains Duplicate-II

--- LeetCode/Array/containsDuplicate-II.java
+++ LeetCode/Array/containsDuplicate-II.java
@@ -14,3 +14,20 @@ class Solution {
         return false;
     }
 }
+
+//Set Approach
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> window = new HashSet<>();
+        for(int i = 0 ; i < nums.length;i++){
+            if(i > k){
+                window.remove(nums[i-k-1]);
+            }
+            if(window.add(nums[i]) == false){
+                return true;
+            }
+        }
+        return false;
+    }
+}
