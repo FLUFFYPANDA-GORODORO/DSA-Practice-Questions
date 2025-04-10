79d696c0bf6004fc3da68224b8e61bf02ad51945
~Gaurav Patil <122665090+FLUFFYPANDA-GORODORO@users.noreply.github.com>
2025-04-10 21:09:31 +0530

    Balanced Binary tree

--- LeetCode/Binart Tree/balancedBinaryTree.java
+++ LeetCode/Binart Tree/balancedBinaryTree.java
@@ -1,3 +1,5 @@
+// Top Down Approach : O(n ^ 2)
+
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -23,5 +25,40 @@ class Solution {
     public int height(TreeNode node){
         if(node == null) return 0;
         return 1 + Math.max(height(node.left) , height(node.right));
+    }
+}
+
+
+// Bottom up Approach : O(n)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        return height(root) != -1;
+    }
+
+    public int height(TreeNode node){
+        if(node == null) return 0;
+        int left = height(node.left);
+        int right = height(node.right);
+        int balanceFactor = Math.abs(left - right);
+
+        if(balanceFactor > 1 || left == -1 || right == -1) return -1;
+        return 1 + Math.max(left , right);
     }
 }
